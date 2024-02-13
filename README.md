696d0de3ece56485761de72290090fa912a8bca2
Lara Frank <lara.frank.frank@yandex.ru>
2024-02-13 17:33:16 +0300

    added create and delete cards logic

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/larfrank/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardData, deleteFunction) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunction);
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+initialCards.forEach((elem) => {
+  const card = createCard(elem, (event) => {
+    event.target.closest('.places__item').remove();
+  });
+  cardContainer.append(card);
+})
